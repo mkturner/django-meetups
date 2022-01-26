0ce0745af1c0e19a934aaadcf02ac619d4f21cbc
Marvin T <contact@marvinkturner.com>
2022-01-26 02:22:53 -0500

    feat ✨: add dynamic info with templates

--- meets/views.py
+++ meets/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'meets/index.html')
+    meets = [
+        {'title': 'First Meetup'},
+        {'title': 'Second Meetup'}
+    ]
+
+    # render accepts dictionary (key-value pair) as 3rd argument
+    # key = name exposed in HTML template
+    # value = python data to expose in template
+    return render(request, 'meets/index.html', {
+        'meet_list': meets,
+        'show_meetups': True
+    })
